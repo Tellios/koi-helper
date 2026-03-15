26439a0a239cfde1615c480d787c81312fa3ddb0
Tellios <sonny.johannesson@gmail.com>
2026-03-15 11:07:05 +0100

    add error-boundary

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -5,7 +5,7 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { logger } from '@shared/logger';
 import { createOvermind } from 'overmind';
 import { Provider } from 'overmind-react';
-import React, { StrictMode } from 'react';
+import React, { PropsWithChildren, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.component';
 import { getConfig } from './state';
@@ -75,16 +75,74 @@ const theme = createTheme({
 
 logger.verbose('Starting app UI');
 
+class ErrorBoundary extends React.Component<
+  PropsWithChildren,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  { hasError: boolean; error: any }
+> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: undefined };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logger.error(`Unexpected crash while rendering app: ${error}\nErrorInfo: ${errorInfo}`);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '8px',
+
+            width: '100vw',
+            height: '100vh',
+          }}
+        >
+          <h1>Unexpected error occured.</h1>
+
+          {this.state.error && (
+            <p>
+              {typeof this.state.error === 'string'
+                ? this.state.error
+                : this.state.error.toString()}
+            </p>
+          )}
+          {!this.state.error && <p>No error details available.</p>}
+
+          <button onClick={() => window.location.reload()}>Reload</button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider value={overmind}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <App />
-          </LocalizationProvider>
-        </CssBaseline>
-      </ThemeProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider value={overmind}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <App />
+            </LocalizationProvider>
+          </CssBaseline>
+        </ThemeProvider>
+      </Provider>
+    </ErrorBoundary>
   </StrictMode>,
 );
